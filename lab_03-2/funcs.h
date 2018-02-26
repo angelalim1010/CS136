b71fcbc3fb566fd372a3735c52f5443ef9cfc079
Angela Lim <angela.lim57@myhunter.cuny.edu>
2018-02-25 19:06:23 -0500

    added the replica

---
+++ lab_03-2/funcs.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+double east_storage(string valid_date);
+
+double mineast();
+
+double maxeast();
+
+string compare(string date1, string date2);
+
+string reverse_order(string date1, string date2);
