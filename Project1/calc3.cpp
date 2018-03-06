af717268d7d0e0524e2ff56539c0fcc524760685
Angela Lim <angela.lim57@myhunter.cuny.edu>
2018-03-05 20:10:11 -0500

    uploaded project

--- Project1/calc3.cpp
+++ Project1/calc3.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 /*
 Author: Angela Lim
@@ -8,65 +7,43 @@ This program will read a file, accept symbols,
 formulas, and non-negative integers. This will be
 able to return the sum of the integers.
 */
-int main() {
-    int number, sum = 0;
-    char op, op2;
-    cin >> number;
-    while (cin >> op) {
-        if (op == ';') {
-            cout << sum << endl;
-            cin >> number;
-            sum = number;
-        } // this returns the first integer
-        else if (op == '^') {
-            cin >> op2;
-            if (op2 == ';') {
-                sum += (number * number);
-                cout << sum << endl;
-                cin >> number;
-                sum = number;
-            }//returns first integer if its being squared
-            else {
-                cin >> number;
-                if (op2 == '+'){
-                    sum += (number*number);
-                } //add the following integers if + is the next symbol
-                else if (op2 == '-'){
-                    sum -=  (number*number); 
-                }// subtract if - is the following symbol
-            }
-        }
-        else {
-            cin >> number >> op2;
-            if (op2 == '^') {
-                if (op == '+'){
-                    sum += (number*number); 
-                }// add if ^ is the second operator
-                else if (op == '-'){
-                    sum -= (number*number); 
-                }// subtract is ^ is the second operator
-            }
-            
-            else if (op == '+') {
-                sum += number;
-                if (op2 == ';'){
-                   cout << sum << endl; 
-                   cin >> number;
-                   sum = number;
-                } //return the sum 
-            }
-            else if (op == '-') {
-                sum -= number;
-
-                if (op2 == ';'){
-                    cout << sum << endl;
-                    cin >> number;
-                    sum = number;
-                } // return a difference
-            }
-            
-        }
+int main(){
+  int number, sum=0; 
+  //result
+  char op; 
+  //operator
+  char op2='+';
+  cin>>number; 
+  //reads in the first number
+  while (cin>>op){
+  //reads in the operator
+    if (op=='^'){ 
+    //square numbers
+      number = number*number;
+      cin>>op;
+      //reads in the next operator
     }
-
-    return 0;
+    if(op2 =='+'){
+      sum += number; 
+      //adds next number to sum
+    }
+    if(op2=='-'){
+      sum -= number; 
+      //subtracts number to sum
+    }
+    op2=op; 
+    //resets the variable
+    cin>>number; 
+    //reads in the numbers
+    if(op2==';'){
+      cout<<sum<<endl; 
+      //prints out sum
+      sum=0; 
+      //resets the sum 
+      op2='+'; 
+      //resets the variable
+    }
+  }
+  return 0;
 }
+    
