ce934c613e829f083e2667494a67007f2e094498
Angela Lim <angela.lim57@myhunter.cuny.edu>
2018-03-21 19:18:40 -0400

    added lab_07 folder/fixed lab_05

---
+++ project_02/pronounce.cpp
@@ -0,0 +1,19 @@
+/*
+Author: Angela Lim
+Course: CSCI-135
+Instructor: Gennadiy Maryash
+Assignment: Project 2
+
+This program will allow the user to input a word
+If the word is part of the dictionary, 
+it will return its pronouncation, words with identical pronouncation,
+words produced if added a phoneme, if you removed a phoneme and replaced a phoneme.
+*/
+#include <iostream>
+#include <string>
+#include "cmudict.0.7a"
+using namespace std;
+
+string w;
+cout << "Enter a word: " << endl;
+cin >> w;
